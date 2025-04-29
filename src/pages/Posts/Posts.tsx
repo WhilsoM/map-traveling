64d5c66ae4f135b86a094b8b21e3765c2c7fff74
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2025-04-29 14:55:07 +0300

    feat: slice to component page posts

--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,3 +1,19 @@
+import { useState } from 'react'
+import { Post } from './Post/Post'
+import s from './posts.module.scss'
+
 export const Posts = () => {
-	return <div>Posts</div>
+	const [posts, setPosts] = useState([{ key: 'd' }, { key: '' }])
+	console.log(posts.length)
+
+	return (
+		<div className={`container ${s.posts}`}>
+			<h2 className={`section-title tac `}>Ваши публикации</h2>
+			{posts.length === 0 ? (
+				<p className={s.emptyPosts}>Постов нету :)</p>
+			) : (
+				posts.map((el) => <Post key={el.key} />)
+			)}
+		</div>
+	)
 }
