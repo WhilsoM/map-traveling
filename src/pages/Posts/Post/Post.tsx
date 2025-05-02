a04aae0eac23b02957f4486343d39ddf6f818e05
ArthurAkhmetov <arthur.akhmetov.2008@gmail.com>
2025-05-02 21:00:56 +0300

    feat(posts): connect with backend and create post with modal and show all posts

--- src/pages/Posts/Post/Post.tsx
+++ src/pages/Posts/Post/Post.tsx
@@ -1,45 +1,46 @@
+import { PostsProps } from '../Posts'
 import s from '../posts.module.scss'
 
-export const Post = () => {
+type PostProps = PostsProps
+
+export const Post = ({
+	img,
+	city,
+	country,
+	dateFrom,
+	dateTo,
+	info,
+}: PostProps) => {
 	return (
 		<article className={s.post}>
 			<div className={s.post__wrapper_img}>
-				<img
-					src='https://img.freepik.com/free-photo/friends-sightseeing-old-town_23-2147762281.jpg?t=st=1745922271~exp=1745925871~hmac=ff1a9a64ea001567e350e02fd5275e64507a8c39349b4dd0741a4350e9fd5937&w=900'
-					alt='Картинка с путешествия'
-					width={500}
-					height={'auto'}
-				/>
+				{img && (
+					<img
+						src={img}
+						alt={`Картинка с города ${city}`}
+						width={500}
+						height={'auto'}
+					/>
+				)}
 			</div>
 
 			<div className={s.post__wrapper_info}>
 				<div className={s.where}>
 					<p>
 						<span>
-							Страна: <span>Россия</span>
+							Страна: <span>{country}</span>
 						</span>
 						<span>
-							Город: <span>Москва</span>
+							Город: <span>{city}</span>
 						</span>
 					</p>
 					<p>
-						Отдыхал с <span>4</span> по <span>28</span> <span>мая</span>{' '}
+						Отдыхал с <span>{dateFrom}</span> по <span>{dateTo}</span>{' '}
 					</p>
 				</div>
 
 				<div className={s.travel_about}>
-					<p>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem
-						consequatur aliquid nostrum tempora molestiae beatae expedita dolor,
-						aliquam blanditiis dolorem? Lorem ipsum dolor sit amet consectetur
-						adipisicing elit. Iste ducimus vel enim doloremque obcaecati fuga
-						nesciunt magni delectus inventore hic. Lorem ipsum dolor sit amet,
-						consectetur adipisicing elit. Quaerat pariatur nam esse repellendus
-						eos perferendis dolor sit iure animi nulla ad sapiente, aperiam
-						maiores commodi at, numquam incidunt eligendi dolore rerum, harum
-						molestias dicta. Ipsam, possimus! Cumque velit fugiat aut sequi!
-						Ipsa totam vitae fuga optio maxime beatae illum sit.
-					</p>
+					<p>{info}</p>
 				</div>
 			</div>
 		</article>
--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -4,20 +4,30 @@ import { CreatePostModal } from './CreatePostModal/CreatePostModal'
 import { Post } from './Post/Post'
 import s from './posts.module.scss'
 
+export type PostsProps = {
+	img?: string
+	city: string
+	country: string
+	dateFrom: string
+	dateTo: string
+	info: string
+}
+
 export const Posts = () => {
-	const [posts, setPosts] = useState([{ key: 'd' }, { key: '' }])
+	const [posts, setPosts] = useState<PostsProps[]>([])
 	const [isOpenModal, setIsOpenModal] = useState(false)
 
 	useEffect(() => {
-		fetchPosts()
+		getAllPosts()
 	}, [])
 
-	const fetchPosts = async () => {
+	const getAllPosts = async () => {
 		try {
-			const response = await fetch('/api/express_backend')
+			const response = await fetch('/api/posts')
 			const data = await response.json()
 
 			console.log(data)
+			setPosts(data)
 		} catch (error) {
 			console.log(error)
 		}
@@ -41,9 +51,19 @@ export const Posts = () => {
 				{posts.length === 0 ? (
 					<p className={s.emptyPosts}>Постов нету :)</p>
 				) : (
-					posts.map((el) => <Post key={el.key} />)
+					posts.map((el) => (
+						<Post
+							key={el.city + el.country}
+							city={el.city}
+							country={el.country}
+							dateFrom={el.dateFrom}
+							dateTo={el.dateTo}
+							info={el.info}
+						/>
+					))
 				)}
 			</section>
+
 			{isOpenModal && (
 				<CreatePostModal
 					isOpenModal={isOpenModal}
