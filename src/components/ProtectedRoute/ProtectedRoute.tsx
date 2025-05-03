f9711c071cee848cdfa90793899d0e1227a7bae2
ArthurAkhmetov <arthur.akhmetov.2008@gmail.com>
2025-05-03 14:59:29 +0300

    feat(posts): who is create post, add lottie animation for loader

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,16 +1,24 @@
 import { auth } from '@api/firebase'
-import React from 'react'
+import catLoader from '@assets/animations/cat.json'
+import Lottie from 'lottie-react'
+import { type ReactNode } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { Navigate } from 'react-router'
 
 interface ProtectedRouteProps {
-	children: React.ReactNode
+	children: ReactNode
 }
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
 	const [user, loading] = useAuthState(auth)
 
-	if (loading) return <div className='loader'>Загрузка...</div>
+	if (loading)
+		return (
+			<div className='loader'>
+				<Lottie animationData={catLoader} loop />
+				<p>Загрузка...</p>
+			</div>
+		)
 
 	return user ? children : <Navigate to={'/auth/register'} />
 }
